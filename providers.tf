94dded7333707d2f6100d272a10187aaaeb8d594
vipin2060 <vipinvikraman86@gmail.com>
2026-02-02 21:43:32 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,4 @@
-
+*/
 terraform {
   required_providers {
     equinix = {
@@ -27,3 +27,34 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+*/
+
+
+
+terraform {
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = "2.4.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.63.1"
+
+    }  
+  }
+}
+
+
+provider "aws" {
+  region = var.primary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+provider "aws" {
+  alias = "secondary"
+  region     = var.secondary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+} 
