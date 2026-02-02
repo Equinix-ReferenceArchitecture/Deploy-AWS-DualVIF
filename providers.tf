d06884d60d12385d4305396bc2ca1565ca6c603b
vipin2060 <vipinvikraman86@gmail.com>
2026-02-02 22:02:30 +0800

    Clean up providers.tf by removing comments
    Removed commented-out provider configurations for AWS.

--- providers.tf
+++ providers.tf
@@ -1,34 +1,3 @@
-/*
-terraform {
-  required_providers {
-    equinix = {
-      source  = "equinix/equinix"
-      version = "2.4.0"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.63.1"
-
-    }  
-  }
-}
-
-
-provider "aws" {
-  region     = "ap-southeast-1" 
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-provider "aws" {
-  alias = "ap-east-1"
-  region     = "ap-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-*/
-
 
 
 terraform {
